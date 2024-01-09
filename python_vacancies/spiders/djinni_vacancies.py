4e102b76905858d41654d0bbce8627e46d47d990
Eugene Rabenko <qwer09846@gmail.com>
2024-01-09 19:03:03 +0200

    add spider for parcing vacancies

---
+++ python_vacancies/spiders/djinni_vacancies.py
@@ -0,0 +1,39 @@
+import scrapy
+from scrapy.http import Response
+
+from python_vacancies.config import PARSE_URL, DOMAIN, TECHNOLOGIES
+
+
+class DjinniVacanciesSpider(scrapy.Spider):
+    name = "vacancies"
+    allowed_domains = [DOMAIN]
+    start_urls = [PARSE_URL]
+
+    def parse(self, response: Response, **kwargs):
+        for vacancy in response.css(".job-list-item"):
+            vacancy_detail_url = vacancy.css(".job-list-item__link::attr(href)").get()
+
+            yield response.follow(vacancy_detail_url, callback=self._parse_single_vacancy)
+
+        next_page = response.css('li.page-item:last-child a.page-link::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+    def _parse_single_vacancy(self, response: Response):
+        yield {
+            "Title": response.css("h1::text").get().strip(),
+            "Technologies": self._find_technologies(response),
+            "Company": response.css(".job-details--title::text").get().strip(),
+            "Salary": response.css(".public-salary-item::text").get(),
+            "English level": response.xpath('//div[contains(text(), "Англійська:")]/text()').get(),
+            "Url": response.url
+        }
+
+    def _find_technologies(self, response: Response) -> list:
+        current_vacancy_stack = []
+
+        for tech in TECHNOLOGIES:
+            if tech.lower() in response.css(".mb-4").get().lower():
+                current_vacancy_stack.append(tech)
+
+        return current_vacancy_stack
